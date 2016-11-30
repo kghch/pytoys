591559ececfc669ad5cdb511ae56aee928e6f890
wangh <wanghan@DESKTOP-3S9VQOF>
2016-11-30 17:01:44 +0800

    Support all-screen preview and save

--- markdown_editor/markdown_editor.py
+++ markdown_editor/markdown_editor.py
@@ -3,14 +3,21 @@ import markdown
 import os
 import tornado.web
 import tornado.httpserver
+import torndb
+import json
 
 MARKDOWN_EXT = ('codehilite', 'extra')
+
+db = torndb.Connection(host='127.0.0.1:3306', database='docs', user='root', password='123456')
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r'/', HomeHandler),
-            (r'/preview', PreviewHandler)
+            (r'/preview', PreviewHandler),
+            (r'/create', CreateHandler),
+            (r'/save', SaveHandler),
+            (r'/show/preview', ShowPreviewHandler)
         ]
 
         settings = dict(
@@ -24,15 +31,47 @@ class Application(tornado.web.Application):
 
 class HomeHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render('home.html', content='Hello world.')
+        latest = db.get("SELECT * FROM doc ORDER BY updated DESC LIMIT 1")
+        if latest:
+            self.render('home.html', fid=latest['fid'], title=latest['title'], raw=latest['raw'], html=latest['html'])
+        else:
+            self.render('home.html', fid='0', title='untitled', raw='', html='')
 
 
 class PreviewHandler(tornado.web.RequestHandler):
     def post(self):
         raw_text = self.request.body
         md = markdown.Markdown(extensions=MARKDOWN_EXT)
-        html_text = md.reset().convert(raw_text)    
+        html_text = md.reset().convert(raw_text)
         self.write(html_text)
+
+class CreateHandler(tornado.web.RequestHandler):
+    def get(self):
+        doc = db.get("SELECT * FROM doc ORDER BY fid DESC LIMIT 1")
+        if doc:
+            doc_id = doc['fid'] + 1
+        else:
+            doc_id = 1
+        self.write({'fid':str(doc_id), 'title': 'untitled'})
+        
+class SaveHandler(tornado.web.RequestHandler):
+    def post(self):
+        data = json.loads(self.request.body)
+        doc = db.get("SELECT * FROM doc WHERE fid=%s", int(data['fid']))
+        if doc:
+            fid = doc['fid']
+            db.execute("UPDATE doc set raw=%s, html=%s, updated=UTC_TIMESTAMP()", data['raw'], data['html'])
+        else:
+            fid = int(data['fid'])
+            db.execute("""INSERT INTO doc(fid, title, raw, html, created, updated) VALUES(%s, %s, %s, %s, UTC_TIMESTAMP(), UTC_TIMESTAMP())""", fid, data['title'], data['raw'], data['html'])
+        self.write(str(fid))
+
+class ShowPreviewHandler(tornado.web.RequestHandler):
+    def get(self):
+        fid = self.get_argument('fid')
+        doc = db.get("SELECT * FROM doc WHERE fid=%s", int(fid))
+        self.render('preview.html', html=doc['html'], title=doc['title'])
+
 
 def main():
     http_server = tornado.httpserver.HTTPServer(Application())
--- markdown_editor/static/main.js
+++ markdown_editor/static/main.js
@@ -13,7 +13,48 @@ function updatePreview() {
   });
 }
 
-$(document).ready(function() {
+function createDoc() {
+  $.ajax({
+    type: 'GET',
+    url: '/create',
+    success: function(data) {
+      $('#raw').val('');
+      $('#mirror').html('');
+      $('#doc_id').html(data['fid']);
+      document.title = data['title'];
+    }
+  });
+}
 
+function preview() {
+  fid = $('#doc_id').html();
+  window.location.replace('/show/preview?fid=' + fid)
+}
+
+function mydocs() {
+  
+}
+
+$(document).ready(function() {
+  $(document).on('keydown', function(e){
+      if(e.ctrlKey && e.which === 83){ // Check for the Ctrl key being pressed, and if the key = [S] (83)
+          raw = $('#raw').val();
+          html = $('#mirror').html();
+          fid = $('#doc_id').html();
+          title = raw.substring(0, 6);
+          $.ajax({
+            type: 'POST',
+            url: '/save',
+            data: JSON.stringify({fid: fid, title: title, raw: raw, html: html}),
+            contentType: 'json',
+            success: function(data) {
+                document.title = title;
+                $('#doc_id').html(data);
+            }
+          });
+          e.preventDefault();
+
+      }
+  });
 
 });
