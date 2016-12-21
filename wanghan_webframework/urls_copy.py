b348aa24609bf44caa4130738ab54a8acdb3bbe4
wangh <wanghan@DESKTOP-3S9VQOF>
2016-12-21 14:02:06 +0800

    add template, v0.1

--- wanghan_webframework/urls_copy.py
+++ wanghan_webframework/urls_copy.py
@@ -1,13 +1,27 @@
-from web_copy import get
+from web_copy import get, view
 
-#@view('index.html')
+
 @get('/')
 def Index():
-    str = 'Index page'
-    return str
+    u1 = {"name": "aaa", "sex": "F"}
+    u2 = {"name": "bbb", "sex": "M"}
+    users = []
+    users.append(u1)
+    users.append(u2)
+    return dict(users=users)
 
-
+@view('/another.html')
 @get('/another')
 def Another():
-    str = 'Another page'
-    return str
+    names = []
+    names.append('wanghan')
+    names.append('kghch')
+    names.append('kouganhen')
+
+    return dict(names=names)
+
+@view('/student.html')
+@get('/student/<studentid>')
+def Student(sid):
+    str = 'This is student %s' % sid
+    return dict(title='Student Page', str=str)
--- wanghan_webframework/web_copy.py
+++ wanghan_webframework/web_copy.py
@@ -2,8 +2,51 @@ from wsgiref.simple_server import make_server
 import logging
 import re
 import types
+import functools
 
 logging.getLogger().setLevel('INFO')
+
+_RESPONSE_STATUSES = {
+    404: 'Not Found',
+    405: 'Method Not Allowed'
+}
+
+class HTTPError(Exception):
+    def __init__(self, code):
+        super(HTTPError, self).__init__()
+        self.status = '%d %s' %(code, _RESPONSE_STATUSES[code])
+
+    def __str__(self):
+        return self.status
+
+    __repr__ = __str__
+
+class Template(object):
+    def __init__(self, template_name, **kwargs):
+        self.template_name = template_name
+        self.model = dict(**kwargs)
+
+
+class Jinja2TemplateEngine(object):
+    def __init__(self, tempate_dir, **kwargs):
+        from jinja2 import Environment, FileSystemLoader
+        kwargs['autoescape'] = True
+        self._env = Environment(loader=FileSystemLoader(tempate_dir), **kwargs)
+
+    def __call__(self, path, data):
+        return self._env.get_template(path).render(**data).encode('utf-8')
+
+
+def view(path):
+    def _decorator(func):
+        @functools.wraps(func)
+        def _wrapper(*args, **kwargs):
+            r = func(*args, **kwargs)
+            if isinstance(r, dict):
+                return Template(path, **r)
+            raise ValueError('Expect return a dict when using @view() decorator.')
+        return _wrapper
+    return _decorator
 
 
 def get(path):
@@ -33,9 +76,39 @@ class Route(object):
         self.handler = fn
 
 
+def convert_regex(path):
+    # /another, /school/teacher/<coursename>, /student/<int:studentid>(useless...)
+    reg = '^'
+    p = path.find('<')
+    if p != -1:
+        reg += path[:p] + '(?P'
+        q = path.find('>')
+        colon = path.find(":")
+        if colon != -1:
+            t = path[p+1:colon]
+            if t == 'int':
+                reg += path[p] + path[colon+1:q+1] + '\d+)'
+            else:
+                print "type not supported"
+        else:
+            reg += path[p:q+1] + '[^/]+)'
+    else:
+        reg +=  path
+    reg += '$'
+    return reg
+
 class WSGIApplication(object):
     def __init__(self):
         self._get_dynamic = []
+        self._template_engine = None
+
+    @property
+    def template_engine(self):
+        return self._template_engine
+
+    @template_engine.setter
+    def template_engine(self, engine):
+        self._template_engine = engine
 
     def add_module(self, module):
         if type(module) != types.ModuleType:
@@ -57,24 +130,41 @@ class WSGIApplication(object):
         server.serve_forever()
 
     def get_application(self):
+
+        def fn_route(request):
+            for func in self._get_dynamic:
+                reg_path = convert_regex(func.path)
+                m = re.match(reg_path, request.path_info)
+                if m:
+                    if request.request_method == func.method:
+                        left = reg_path.find('<')
+                        if left != -1:
+                            left += 1
+                            right = reg_path.find('>')
+                            arg = m.group(reg_path[left:right])
+                        else:
+                            arg = None
+                    else:
+                        raise HTTPError(405)
+                    if arg:
+                        return func.handler(arg)
+                    else:
+                        return func.handler()
+            raise HTTPError(404)
+
+
         def wsgi(env, start_response):
             request = Request(env)
 
-            start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+            try:
+                r = fn_route(request)
+                r = self._template_engine(r.template_name, r.model)
 
-            res = ''
-
-            for func in self._get_dynamic:
-                if re.match(func.path + "$", request.path_info):
-                    if request.request_method == func.method:
-                        print func.handler.__name__
-                        res += func.handler()
-                    else:
-                        # TODO: raise 405 Error here
-                        logging.error("Method not allowed, 405 Error")
-                    break
-
-            return [res]
+                start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+                return r
+            except HTTPError, e:
+                start_response(e.status, [('Content-Type', 'text/html; charset=utf-8')])
+                return [e.status]
 
         return wsgi
 
